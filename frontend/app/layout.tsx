fcdd665e9a8b5437c66352768c325e2b23e28f8b
Dateyes-glitch <kyriaki.forfoli@acg.edu>
2026-02-04 14:35:40 +0100

    dfs

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'Pop Head Wars Game Platform',
+  description: 'Pop Head Wars Game Platform',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
